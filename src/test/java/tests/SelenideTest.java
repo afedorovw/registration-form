08c9d5d3257e514248a984f21ee61bb8ae5f5b95
afedorovw <a.fedorovw@gmail.com>
2021-07-13 16:51:08 +0300

    add Allure

--- src/test/java/tests/SelenideTest.java
+++ src/test/java/tests/SelenideTest.java
@@ -2,19 +2,36 @@ package tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.Step;
 
 public class SelenideTest {
 
-    @Test
-    void shouldFindSelenideGitHub() {
+    private static final String Base_URL = "https://github.com";
+    private static final String REPOSITORY = "selenide";
+
+
+    @Step("Открываем главную страницу")
+    public void shouldFindSelenideGitHub(String base_URL) {
         //Configuration.timeout=2000;
-        Selenide.open("https://github.com");
-        //Configuration.baseUrl="https://github.com";
-        //Selenide.open("/user");
-        // $ = element
-        Selenide.$("[name=q]").setValue("selenide").pressEnter();
+        Selenide.open(Base_URL);
+    }
+
+        /* Configuration.baseUrl="https://github.com";
+        Selenide.open("/user");
+         $ = element */
+
+    @Step("Ищем selenide ${repository}")
+    public void searchForRepository(String repository) {
+        Selenide.$("[name=q]").setValue(REPOSITORY).pressEnter();
+    }
+
+    @Step("Выбираем первый пункт в списке")
+    public void goToRepository() {
         Selenide.$$("ul.repo-list li").first().$("a").click();
+    }
+
+    @Step("Проверяем")
+    public void shouldSearch() {
         Selenide.$("h1").shouldHave(Condition.text("selenide / selenide"));
     }
 }
